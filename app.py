c8cacfff5af70439c095af5518163c8ccb8a191f
Noth <alexander@webbmasters.se>
2024-11-09 16:49:20 +0100

    Update app.py

--- app.py
+++ app.py
@@ -146,3 +146,4 @@ if __name__ == '__main__':
     init_db()  # Initialize the database on startup if it doesn't exist
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
+
