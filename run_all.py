59f5042a5da65c703b8f6076d0c6a4c144880204
Noth <alexander@webbmasters.se>
2024-11-09 23:14:21 +0100

    Update run_all.py

--- run_all.py
+++ run_all.py
@@ -1,7 +1,7 @@
 import subprocess
 
 # Start EVAC Map app on port 5001
-subprocess.Popen(['python3', 'evac-app.py'])
+subprocess.Popen(['python3', 'evacmap/evac-app.py'])
 
 # Start Draft Map app on port 5002
-subprocess.Popen(['python3', 'draft-app.py'])
+subprocess.Popen(['python3', 'draftmap/draft-app.py'])
