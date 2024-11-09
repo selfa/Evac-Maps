d25401afafa8bcc08d2c1f431c537cc8d0565c5d
Noth <alexander@webbmasters.se>
2024-11-09 20:15:40 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import os
 app = Flask(__name__, static_url_path='/static', static_folder='static', template_folder='templates')
 
 # Path to the SQLite database
-EVAC_DATABASE = 'markerss.db'
+EVAC_DATABASE = 'markers.db'
 DRAFT_DATABASE = 'app_state.db'
 
 # Function to initialize the evac map database (markers.db)
