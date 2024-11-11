d39c0b6dbec85fd5ab82732f60ed0e377969637b
Noth <alexander@webbmasters.se>
2024-11-11 19:31:03 +0100

    fe

--- cce/cca.py
+++ cce/cca.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static')
 
-@app.route('/cce')
+@app.route('/cca')
 def lootpath_page():
-    return render_template('cce.html')
+    return render_template('cca.html')
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=8004)
