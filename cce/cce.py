32b53dd44166a94f5833d33c7af44985c5c783ec
Noth <alexander@webbmasters.se>
2024-11-11 19:28:45 +0100

    Update cce.py

--- cce/cce.py
+++ cce/cce.py
@@ -7,4 +7,4 @@ def lootpath_page():
     return render_template('cce.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=8003)
+    app.run(debug=True, host='0.0.0.0', port=8004)
